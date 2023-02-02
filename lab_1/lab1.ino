d0546c55d2bb7efc1b627c39de69f2486aac4081
emillantz <emil.lantz@gmail.com>
2023-02-02 13:01:00 +0100

    fixed lab1 code (i think)

--- lab_1/lab1.ino
+++ lab_1/lab1.ino
@@ -1,6 +1,7 @@
 #include <stdio.h>
 // 1
-byte tx_bit = (tx.frame >> i - 1) & 0x01;
+byte tx_bit = (tx.frame << i) & 0x80000000;
+(tx_bit == 0x80000000) ? tx_bit = 0x1 : tx_bit = 0x0;
 
 // 2
 const byte PREAMBLE = 0b10101010;
@@ -8,8 +9,8 @@ const int PIN_RX = 0;
 const int PIN_TX = 13;
 for (int i = 0; i < 8; i++)
 {
-    tx_bit = (PREAMBLE >> i - 1) & 0x01;
-    digitalWrite(PIN_TX, tx_bit);
+    tx_bit = (PREAMBLE << i) & 0x80;
+    (tx_bit == 0x80) ? digitalWrite(PIN_TX, 0x1) : digitalWrite(PIN_TX, 0x0);
     delay(100);
 }
 
@@ -17,4 +18,5 @@ for (int i = 0; i < 8; i++)
 rx.frame = (rx.frame << 1) | rx_bit;
 
 // 4
-tx.frame_payload = Shield::selectLed();
+Shield sh();
+tx.frame_payload = sh.selectLed();
