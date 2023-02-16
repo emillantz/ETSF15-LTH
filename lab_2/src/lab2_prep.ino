db71870f794d640de6ac12ef65da01c8e2f58401
emillantz <emil.lantz@gmail.com>
2023-02-16 12:28:10 +0100

    fk it

--- lab_2/src/lab2_prep.ino
+++ lab_2/src/lab2_prep.ino
@@ -3,18 +3,18 @@
 
 const byte DIVISOR = 0xA7;
 // I think this works??
-byte Compute_CRC8(byte *bytes, int len)
+byte Compute_CRC8(byte input)
 {
-    byte crc = 0;
+    byte crc = input;
 
-    while (len--)
+    for (int i = 0; i < 8; i++)
     {
-        crc ^= *bytes++;
-
-        for (int i = 0; i < 8; i++)
-        {
-            ((crc & 0x80) != 0) ? (crc = (byte)((crc << 1) ^ DIVISOR)) : (crc <<= 1);
-        }
+        ((crc & 0x80) != 0) ? (crc = (byte)((crc << 1) ^ DIVISOR)) : (crc <<= 1);
     }
     return crc;
+}
+
+boolean check_CRC8(byte input)
+{
+    return Compute_CRC8(input) == 0;
 }
